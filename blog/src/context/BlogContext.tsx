3db4f575a624b36c8876d3b5be6d4eb97c159b52
g-vega-cl <g.vega.cl@gmail.com>
2021-03-08 10:17:25 -0600

    Finished creating context. No reducer

--- blog/src/context/BlogContext.tsx
+++ blog/src/context/BlogContext.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from "react";
 
+const BlogContext = React.createContext({
+  blogPosts: [{ title: "" }],
+  addBlogPost: () => {},
+});
 
-const BlogContext = React.createContext({});
-
-export const BlogProvider: React.FC = ({children}) => {
-    return (
-    <BlogContext.Provider value={5}>
-        {children}
-    </BlogContext.Provider>
-    )
+interface listItem {
+  title: string;
 }
+
+export const BlogProvider: React.FC = ({ children }) => {
+  const [blogPosts, setBlogPosts] = useState<listItem[]>([{ title: "Blog 1" }, { title: "Blog 2" }]);
+
+  const addBlogPost = () => {
+    setBlogPosts([
+      ...blogPosts,
+      { title: `Blog Post #${blogPosts.length + 1}` },
+    ]);
+  };
+
+  const contextObject = {
+    blogPosts,
+    addBlogPost,
+  };
+
+  
+
+  return (
+    <BlogContext.Provider value={contextObject}>
+      {children}
+    </BlogContext.Provider>
+  );
+};
 
 export default BlogContext;
--- blog/src/screens/IndexScreen.tsx
+++ blog/src/screens/IndexScreen.tsx
@@ -1,12 +1,36 @@
-import React, {useContext} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useContext, useEffect} from 'react';
+import { Button, View, Text, StyleSheet } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import BlogContext from '../context/BlogContext';
 
+interface listItem{
+    item: {
+        title: string
+    }
+}
+
 const IndexScreen = () => {
-    const value = useContext(BlogContext);
+    const fullContext = useContext(BlogContext);
+    const blogPosts = fullContext.blogPosts;
+    const addBlogPost = fullContext.addBlogPost;
+    
+    
+    const renderBlogPostsList: React.FC<listItem> = ({item}) => {
+        return(<Text>{item.title}</Text>)
+    }
+
     return (
         <View>
             <Text>Index</Text>
+            <Button
+                title="Add Post"
+                onPress={addBlogPost}
+            />
+            <FlatList
+                data = {blogPosts}
+                keyExtractor = {(blogPost) => blogPost.title}
+                renderItem={renderBlogPostsList}
+            />
         </View>
     )
 }
